641b3a9464bbc19cccacde849abad9d27abbff31
Xavman42 <xavieredavenport@hotmail.com>
2023-10-05 16:06:56 -0500

    Following paper, I've filtered NSynth samples from 289205 to 154311 samples. Only ~53000 left to go!

--- Natsiou_Model/PyTorch/helper_NSynth.py
+++ Natsiou_Model/PyTorch/helper_NSynth.py
@@ -12,10 +12,53 @@ def parse_json_metadata():
     datapipe2 = FileOpener(datapipe1, mode="b")
     datapipe3 = datapipe2.map(get_name)
     json_dp = datapipe3.parse_json_files()
-    return list(json_dp)
+    json_list = list(json_dp)
+    return json_list[0][1]
+
+
+def filter_json_metadata_pitch(old_dict):
+    new_dict = {}
+    for key in old_dict.keys():
+        if old_dict[key]['pitch'] < 40 or old_dict[key]['pitch'] > 96:
+            pass
+        else:
+            new_dict[key] = old_dict[key]
+    print("Previous Sample Set Size: ", "->", len(old_dict))
+    print("New Sample Set Size: ", "->", len(new_dict))
+    return new_dict
+
+
+def filter_json_metadata_instrument_family(old_dict):
+    new_dict = {}
+    for key in old_dict.keys():
+        if (old_dict[key]['instrument_family_str'] == 'bass' or
+                old_dict[key]['instrument_family_str'] == 'brass' or
+                old_dict[key]['instrument_family_str'] == 'flute' or
+                old_dict[key]['instrument_family_str'] == 'guitar' or
+                old_dict[key]['instrument_family_str'] == 'keyboard' or
+                old_dict[key]['instrument_family_str'] == 'organ' or
+                old_dict[key]['instrument_family_str'] == 'reed' or
+                old_dict[key]['instrument_family_str'] == 'string'):
+            new_dict[key] = old_dict[key]
+    print("Previous Sample Set Size: ", "->", len(old_dict))
+    print("New Sample Set Size: ", "->", len(new_dict))
+    return new_dict
+
+
+def filter_json_metadata_quality(old_dict):
+    new_dict = {}
+    for key in old_dict.keys():
+        if "percussive" in old_dict[key]['qualities_str']:
+            pass
+        else:
+            new_dict[key] = old_dict[key]
+    print("Previous Sample Set Size: ", "->", len(old_dict))
+    print("New Sample Set Size: ", "->", len(new_dict))
+    return new_dict
 
 
 if __name__ == '__main__':
-    json_list = parse_json_metadata()
-    for key in json_list[0][1]:
-        print(key, "->", json_list[0][1][key])
+    json_dict = parse_json_metadata()
+    json_dict = filter_json_metadata_pitch(json_dict)
+    json_dict = filter_json_metadata_instrument_family(json_dict)
+    json_dict = filter_json_metadata_quality(json_dict)
