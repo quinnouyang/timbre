c71cb570e6d20b61d8b641f228bc2c3af885ceaf
Xavman42 <xavieredavenport@hotmail.com>
2023-10-13 10:55:02 -0500

    I think I'm correctly plotting the waveform and spectrogram, I just can't get the plot to show

--- Natsiou_Model/PyTorch/helper_NSynth.py
+++ Natsiou_Model/PyTorch/helper_NSynth.py
@@ -2,20 +2,27 @@
 # from torchaudio.transforms import SpectralCentroid
 import random
 
+import librosa
 import torchaudio
 from pathlib import Path
 from torchaudio import transforms
 from torchaudio import functional
 from torchdata.datapipes.iter import IterableWrapper, FileOpener
 from torch.utils.data import Dataset
+# from audiolazy import lazy_midi
+import torch
+import matplotlib.pyplot as plt
 import os
+from IPython.display import Audio
 
 
 class NSynthDataset(Dataset):
 
-    def __init__(self, annotations_json, audio_dir):
+    def __init__(self, annotations_json, audio_dir, transformation, target_sample_rate):
         self.annotations = annotations_json
         self.audio_dir = audio_dir
+        self.transformation = transformation
+        self.target_sample_rate = target_sample_rate
 
     def __len__(self):
         return len(self.annotations)
@@ -24,14 +31,44 @@ class NSynthDataset(Dataset):
         audio_sample_path = self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
         signal, sr = torchaudio.load(audio_sample_path)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self.transformation(signal)
         return signal, label
 
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
+
+    def _mix_down_if_necessary(self, signal):
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
+
     def _get_audio_sample_path(self, index):
-        path = os.path.join(self.audio_dir, list(self.annotations.items())[index][1]["instrument_str"], ".wav")
+        path = self.audio_dir + "/" + list(self.annotations.items())[index][1]["note_str"] + ".wav"
         return path
 
     def _get_audio_sample_label(self, index):
-        return list(self.annotations.items())[index][1]["instrument_str"]
+        return list(self.annotations.items())[index][1]["note_str"]
+
+    # def _get_fundamental_freq(self, index):
+    #     return lazy_midi.midi2freq(list(self.annotations.items())[index][1]["pitch"])
+
+    def get_harmonic_amplitude(self, index, num_harmonics):
+        # fund = self._get_fundamental_freq(self, index)
+        sample = librosa.load(self._get_audio_sample_path(index))
+        hop_length = 512
+        n_fft = 2048
+        fts = librosa.stft(sample, n_fft=n_fft, hop_length=hop_length)
+        S = librosa.amplitude_to_db(abs(fts))
+        # for i in range(num_harmonics-1):
+        #     harm_freq = fund * (i+1)
+        plt.figure(figsize=(15, 5))
+        librosa.display.specshow(S, self.sr, hop_length, x_axis="time", y_axis="linear")
+        plt.colorbar(format='%+2.0f dB')
 
     def get_random_annotation(self):
         return random.choice(list(self.annotations.items()))[1]
@@ -104,20 +141,49 @@ def calculate_spectral_centroid():
     print(spectral_centroid)
 
 
-def calculate_fundamental_frequency():
-    waveform, sample_rate = torchaudio.load(Path('nsynth-train/audio/guitar_acoustic_001-082-050.wav').resolve(),
-                                            normalize=True)
-    pitch = functional.detect_pitch_frequency(waveform, sample_rate)
-    print(pitch)
+def plot_spectrogram(specgram, title=None, ylabel="freq_bin", ax=None):
+    if ax is None:
+        _, ax = plt.subplots(1, 1)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto", interpolation="nearest")
+
+
+def plot_waveform(waveform, sr, title="Waveform", ax=None):
+    waveform = waveform.numpy()
+    num_channels, num_frames, question = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sr
+    if ax is None:
+        _, ax = plt. subplots(num_channels, 1)
+    ax.plot(time_axis, waveform[0], linewidth=1)
+    ax.grid(True)
+    ax.set_xlim([0, time_axis[-1]])
+    ax.set_title(title)
 
 
 if __name__ == '__main__':
+    SAMPLE_RATE = 16000
     json_dict = parse_json_metadata()
     json_dict = filter_json_metadata_pitch(json_dict)
     json_dict = filter_json_metadata_instrument_family(json_dict)
     json_dict = filter_json_metadata_quality(json_dict)
-    data = NSynthDataset(json_dict, "/nsynth-train/audio")
+
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+    data = NSynthDataset(json_dict, "./nsynth-train/audio", mel_spectrogram, SAMPLE_RATE)
     point = data.get_random_annotation()
     print(point)
+    signal, label = data[0]
+    fig, axs = plt.subplots(2, 1)
+    plot_waveform(signal, SAMPLE_RATE, title="Original waveform", ax=axs[0])
+    plot_spectrogram(signal[0], title="MelSpectrogram - torchaudio", ylabel="mel freq")
+    fig.tight_layout()
+    # plt.show
+    # data.get_harmonic_amplitude(0, 7)
     # calculate_spectral_centroid()
     # calculate_fundamental_frequency()
