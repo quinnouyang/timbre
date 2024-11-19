fc105d8989184f77992129da3e28eca4ccc840ac
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-11-19 21:55:59 +0000

    [model v2] Init

---
+++ model_v2/dataclass.py
@@ -0,0 +1,26 @@
+import torch
+
+from dataclasses import dataclass
+
+
+@dataclass
+class VAEOutput:
+    """
+    Dataclass for VAE output.
+
+    Attributes:
+        z_dist (torch.distributions.Distribution): The distribution of the latent variable z.
+        z_sample (torch.Tensor): The sampled value of the latent variable z.
+        x_recon (torch.Tensor): The reconstructed output from the VAE.
+        loss (torch.Tensor): The overall loss of the VAE.
+        loss_recon (torch.Tensor): The reconstruction loss component of the VAE loss.
+        loss_kl (torch.Tensor): The KL divergence component of the VAE loss.
+    """
+
+    z_dist: torch.distributions.Distribution
+    z_sample: torch.Tensor
+    x_recon: torch.Tensor
+
+    loss: torch.Tensor
+    loss_recon: torch.Tensor
+    loss_kl: torch.Tensor
---
+++ model_v2/vae.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+from .dataclass import VAEOutput
+
+
+class VAE(nn.Module):
+    """
+    Variational Autoencoder (VAE) class.
+
+    Args:
+        input_dim (int): Dimensionality of the input data.
+        hidden_dim (int): Dimensionality of the hidden layer.
+        latent_dim (int): Dimensionality of the latent space.
+    """
+
+    def __init__(self, input_dim: int, hidden_dim: int, latent_dim: int) -> None:
+        super(VAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 2, hidden_dim // 4),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 4, hidden_dim // 8),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 8, hidden_dim // 16),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 16, hidden_dim // 32),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 32, hidden_dim // 64),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 64, hidden_dim // 128),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 128, hidden_dim // 256),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 256, 2 * latent_dim),  # 2 for mean and variance.
+        )
+        self.softplus = nn.Softplus()  # Smooth-RELU to constraint output to (0, +inf)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim // 256),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 256, hidden_dim // 128),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 128, hidden_dim // 64),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 64, hidden_dim // 32),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 32, hidden_dim // 16),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 16, hidden_dim // 8),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 8, hidden_dim // 4),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 4, hidden_dim // 2),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 2, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid(),
+        )
+
+        print("\nEncoder layers:")
+        print(self.encoder)
+        print("Decoder layers:")
+        print(self.decoder)
+        print()
+
+    def encode(self, x: torch.Tensor, eps: float = 1e-8) -> MultivariateNormal:
+        """
+        Encodes the input data into the latent space.
+
+        Args:
+            x (torch.Tensor): Input data.
+            eps (float): Small value to avoid numerical instability.
+
+        Returns:
+            torch.distributions.MultivariateNormal: Normal distribution of the encoded data.
+        """
+        x = self.encoder(x)
+        mu, logvar = torch.chunk(x, 2, dim=-1)  # Mean and log-variance
+        scale = self.softplus(logvar) + eps
+        scale_tril = torch.diag_embed(scale)
+
+        return MultivariateNormal(mu, scale_tril=scale_tril)
+
+    def reparameterize(self, dist: MultivariateNormal):
+        """
+        Reparameterizes the encoded data to sample from the latent space.
+
+        Args:
+            dist (torch.distributions.MultivariateNormal): Normal distribution of the encoded data.
+        Returns:
+            torch.Tensor: Sampled data from the latent space.
+        """
+        return dist.rsample()
+
+    def decode(self, z: torch.Tensor):
+        """
+        Decodes the data from the latent space to the original input space.
+
+        Args:
+            z (torch.Tensor): Data in the latent space.
+
+        Returns:
+            torch.Tensor: Reconstructed data in the original input space.
+        """
+        return self.decoder(z)
+
+    def forward(self, x: torch.Tensor, compute_loss: bool = True) -> VAEOutput:
+        """
+        Performs a forward pass of the VAE.
+
+        Args:
+            x (torch.Tensor): Input data.
+            compute_loss (bool): Whether to compute the loss or not.
+
+        Returns:
+            VAEOutput: VAE output dataclass.
+        """
+        dist = self.encode(x)
+        z = self.reparameterize(dist)
+        recon_x = self.decode(z)
+
+        if not compute_loss:
+            return VAEOutput(
+                z_dist=dist,
+                z_sample=z,
+                x_recon=recon_x,
+                loss=None,
+                loss_recon=None,
+                loss_kl=None,
+            )
+
+        # compute loss terms
+        loss_recon = (
+            F.binary_cross_entropy(recon_x, x + 0.5, reduction="none").sum(-1).mean()
+        )
+        std_normal = torch.distributions.MultivariateNormal(
+            torch.zeros_like(z, device=z.device),
+            scale_tril=torch.eye(z.shape[-1], device=z.device)
+            .unsqueeze(0)
+            .expand(z.shape[0], -1, -1),
+        )
+        loss_kl = torch.distributions.kl.kl_divergence(dist, std_normal).mean()
+
+        loss = loss_recon + loss_kl
+
+        return VAEOutput(
+            z_dist=dist,
+            z_sample=z,
+            x_recon=recon_x,
+            loss=loss,
+            loss_recon=loss_recon,
+            loss_kl=loss_kl,
+        )
