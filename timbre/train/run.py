042af82b147863d53435bbed336100c5bf68b999
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-12-06 16:19:27 -0600

    [train] Correct config imports and paths

--- timbre/train/run.py
+++ timbre/train/run.py
@@ -4,51 +4,82 @@ from torch.optim.adamw import AdamW
 from torch.utils.data import DataLoader
 
 from timbre.data.nsynth import NSynthDataset
-from timbre import train, test, plot, VAE
+from timbre.train.utils import train, test, plot
+from timbre.model.vae import VAE
 
 
-if __name__ == "__main__":
+def main() -> None:
+    print(timbre.DEVICE)
     print("Loading datasets and dataloaders...")
+    # [TODO] Generalize datasets and dataloaders
+    # [TODO] Feature extraction
     TRAIN_DATA = NSynthDataset(
-        DATASETS_DIR / "nsynth" / "nsynth-valid" / "examples.json",
-        DATASETS_DIR / "nsynth" / "nsynth-valid" / "audio",
+        timbre.SOURCES_DIR / "nsynth" / "nsynth-valid" / "examples.json",
+        timbre.SOURCES_DIR / "nsynth" / "nsynth-valid" / "audio",
     )
     TEST_DATA = NSynthDataset(
-        DATASETS_DIR / "nsynth" / "nsynth-test" / "examples.json",
-        DATASETS_DIR / "nsynth" / "nsynth-test" / "audio",
+        timbre.SOURCES_DIR / "nsynth" / "nsynth-test" / "examples.json",
+        timbre.SOURCES_DIR / "nsynth" / "nsynth-test" / "audio",
     )
     TRAIN_LOADER = DataLoader(
         TRAIN_DATA,
-        batch_size=BATCH_SIZE,
+        batch_size=timbre.BATCH_SIZE,
         shuffle=True,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
+        num_workers=timbre.NUM_WORKERS,
+        pin_memory=timbre.USE_PIN_MEMORY,
     )
     TEST_LOADER = DataLoader(
         TEST_DATA,
-        batch_size=BATCH_SIZE,
+        batch_size=timbre.BATCH_SIZE,
         shuffle=False,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
+        num_workers=timbre.NUM_WORKERS,
+        pin_memory=timbre.USE_PIN_MEMORY,
     )
     print(
         f"Training datapoints: {len(TRAIN_DATA)}\nTesting datapoints: {len(TEST_DATA)}\n"
     )
 
     print("Initiating model, optimizer, and Tensorboard...")
-    MODEL = VAE(INPUT_DIM, HIDDEN_DIM, LATENT_DIM).to(DEVICE)
-    OPT = AdamW(MODEL.parameters(), weight_decay=WEIGHT_DECAY)
+    MODEL = VAE(timbre.INPUT_DIM, timbre.HIDDEN_DIM, timbre.LATENT_DIM).to(
+        timbre.DEVICE
+    )
+    OPT = AdamW(MODEL.parameters(), weight_decay=timbre.WEIGHT_DECAY)
 
     print("Entering train-test loop...\n")
     prev_updates = 0
-    for epoch in range(N_EPOCHS):
-        print(f"Epoch {epoch+1}/{N_EPOCHS}")
+    for epoch in range(timbre.NUM_EPOCHS):
+        print(f"Epoch {epoch+1}/{timbre.NUM_EPOCHS}")
         prev_updates = train(
-            MODEL, TRAIN_LOADER, OPT, prev_updates, DEVICE, BATCH_SIZE, WRITER
+            MODEL,
+            TRAIN_LOADER,
+            OPT,
+            prev_updates,
+            timbre.DEVICE,
+            timbre.BATCH_SIZE,
+            timbre.WRITER,
         )
-        test(MODEL, TEST_LOADER, prev_updates, DEVICE, LATENT_DIM, WRITER)
+        test(
+            MODEL,
+            TEST_LOADER,
+            prev_updates,
+            timbre.DEVICE,
+            timbre.LATENT_DIM,
+            timbre.WRITER,
+        )
 
     print("\nPlotting...")
-    plot(MODEL, TRAIN_LOADER, DEVICE, LATENT_DIM, RUNS_DIR, DATETIME_NOW)
+    plot(
+        MODEL,
+        TRAIN_LOADER,
+        timbre.DEVICE,
+        timbre.LATENT_DIM,
+        timbre.RUNS_DIR,
+        timbre.DATETIME_NOW,
+    )
 
     print("Done.")
+
+
+if __name__ == "__main__":
+    # [TODO] Arguments
+    main()
--- timbre/train/utils.py
+++ timbre/train/utils.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard.writer import SummaryWriter
 from tqdm import tqdm
 
-from ..vae import VAE
+from ..model.vae import VAE
 
 
 def train(
